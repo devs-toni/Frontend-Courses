3881f409e80147c334479b4964e548d1ff7173b4
Antonio Rufino <devs.toni@gmail.com>
2022-09-22 01:16:37 +0200

    manipulación de estilos css en el DOM

--- lrn-javascript/jonmircha/#dom/dom1.js
+++ lrn-javascript/jonmircha/#dom/dom1.js
@@ -27,22 +27,41 @@
 //console.log(document.querySelectorAll('.cards').forEach((e) => console.log(e.children)));
 //console.log(document.querySelectorAll('#menu li'))
 
-console.log(document.documentElement.lang);
-console.log(document.querySelector('.link-dom').getAttribute('data-descripcion'));
-console.log(document.querySelector('.link-dom').href);
-document.documentElement.setAttribute('lang','es-ES');
-console.log(document.documentElement.lang);
-
+//console.log(document.documentElement.lang);
+//console.log(document.querySelector('.link-dom').getAttribute('data-descripcion'));
+//console.log(document.querySelector('.link-dom').href);
+//document.documentElement.setAttribute('lang','es-ES');
+//console.log(document.documentElement.lang);
+//
 const $linkDom = document.querySelector('.link-dom');
+//
+//$linkDom.setAttribute('target','_blank');
+//$linkDom.setAttribute('rel','noopener');
+//$linkDom.setAttribute('href','https://www.google.es');
+//
+//console.log($linkDom.hasAttribute('rel'));
+//$linkDom.removeAttribute('rel');
+//console.log($linkDom.hasAttribute('rel'));
+//console.log($linkDom.dataset);
+//$linkDom.dataset.nombre = "Ying";
+//console.log($linkDom.dataset);
+console.log($linkDom.style);
+console.log($linkDom.getAttribute('style'));
+console.log($linkDom.style.color);
+console.log(getComputedStyle($linkDom).getPropertyValue("color"));
+$linkDom.style.setProperty('text-decoration','none');
+$linkDom.style.width = '50%';
+$linkDom.style.marginLeft = '20%'
+$linkDom.style.removeProperty('background-color');
 
-$linkDom.setAttribute('target','_blank');
-$linkDom.setAttribute('rel','noopener');
-$linkDom.setAttribute('href','https://www.google.es');
+// Variables CSS - Custom Properties
 
-console.log($linkDom.hasAttribute('rel'));
-$linkDom.removeAttribute('rel');
-console.log($linkDom.hasAttribute('rel'));
-console.log($linkDom.dataset);
-$linkDom.dataset.nombre = "Ying";
-console.log($linkDom.dataset);
-
+const $html = document.documentElement, $body = document.body;
+let varDarkColor = getComputedStyle($html).getPropertyValue('--dark-color');
+let varYellowColor = getComputedStyle($html).getPropertyValue('--yellow-color');
+console.log(varDarkColor, varYellowColor);
+$body.style.color = varYellowColor;
+$body.style.backgroundColor = varDarkColor;
+$html.style.setProperty('--dark-color', 'pink');
+varDarkColor = getComputedStyle($html).getPropertyValue('--dark-color');
+$body.style.setProperty("background-color", varDarkColor);
