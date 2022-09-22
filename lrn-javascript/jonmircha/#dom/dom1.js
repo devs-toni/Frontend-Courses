57fe0d288da19eaf7edad2660f2c006ecf02e86e
Antonio Rufino <devs.toni@gmail.com>
2022-09-23 00:30:53 +0200

    creación de elementos y fragmentos

--- lrn-javascript/jonmircha/#dom/dom1.js
+++ lrn-javascript/jonmircha/#dom/dom1.js
@@ -90,17 +90,78 @@ $whatIsDOM.textContent = text;
 //$whatIsDOM.outerHTML = text;
 $whatIsDOM.innerHTML = text;
 const $cards = document.querySelector('.multicards');
-console.log($cards.children);
-console.log($cards.children[2].closest('section'));
-console.log($cards.childNodes);
-console.log($cards.parentElement);
-console.log($cards.parentNode);
-console.log($cards.firstChild);
-console.log($cards.firstElementChild);
-console.log($cards.lastChild);
-console.log($cards.lastElementChild);
-console.log($cards.nextSibling);
-console.log($cards.nextElementSibling);
-console.log($cards.previousSibling);
-console.log($cards.previousElementSibling);
-console.log($cards.closest('body'));
+//console.log($cards.children);
+//console.log($cards.children[2].closest('section'));
+//console.log($cards.childNodes);
+//console.log($cards.parentElement);
+//console.log($cards.parentNode);
+//console.log($cards.firstChild);
+//console.log($cards.firstElementChild);
+//console.log($cards.lastChild);
+//console.log($cards.lastElementChild);
+//console.log($cards.nextSibling);
+//console.log($cards.nextElementSibling);
+//console.log($cards.previousSibling);
+//console.log($cards.previousElementSibling);
+//console.log($cards.closest('body'));
+
+const $section = document.createElement('section')
+,$figure = document.createElement('figure'),
+$img = document.createElement('img'),
+$figcaption = document.createElement('figcaption'),
+$figcaptionText = document.createTextNode('Animals'),
+$cardsElement = document.querySelector('.multicards'); 
+
+$figcaption.appendChild($figcaptionText);
+$figure.classList.add('card');
+$section.classList.add('cards');
+$img.setAttribute('src', 'https://placeimg.com/200/200/animals');
+$img.setAttribute('alt', 'Animals');
+$figure.appendChild($img);
+$figure.appendChild($figcaption);
+$section.appendChild($figure);
+$cardsElement.appendChild($section);
+
+const $section2 = document.createElement('section');
+$section2.classList.add('cards');
+$section2.innerHTML = `
+<figure class="card">
+<img src='https://placeimg.com/200/200/people' alt='people' />
+<figcaption>People</figcaption>
+</figure>
+`; 
+
+$cardsElement.appendChild($section2);
+
+const estaciones = ['Invierno','Primavera','Verano','Otoño'];
+const $ul = document.createElement('ul');
+document.write('<h3>Estaciones del Año</h3>');
+
+estaciones.forEach(e => {
+    const $li = document.createElement('li'); 
+    $li.textContent = e ;
+    $ul.appendChild($li);
+});
+document.body.appendChild($ul);
+
+
+const continentes = ['Europa','Africa','America','Asia', 'Oceania'];
+const $ul2 = document.createElement('ul');
+document.write('<h3>Continentes del Mundo</h3>');
+document.body.appendChild($ul2);
+continentes.forEach(e => {
+    $ul2.innerHTML += `<li>${e}</li>`;
+});
+
+const meses = ['Enero','Febrero', 'Marzo','Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre','Noviembre','Diciembre'];
+const $ul3 = document.createElement('ul');
+const $frag = document.createDocumentFragment();
+
+meses.forEach(e => {
+    const $li = document.createElement('li');
+    $li.textContent = e;
+    $frag.appendChild($li);
+})
+document.write('<h3>Meses del Año</h3>');
+$ul3.appendChild($frag);
+document.body.appendChild($ul3);
