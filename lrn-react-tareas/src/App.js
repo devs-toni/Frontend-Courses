5ccf37a5209fba64cddac2421811273bb01420dc
Antonio Rufino <devs.toni@gmail.com>
2022-10-26 16:36:06 +0200

    Eliminación de logo ajeno

--- lrn-react-tareas/src/App.js
+++ lrn-react-tareas/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import logo from './imagenes/Logo.jpg';
 import { Tarea } from './componentes/Tarea';
 import { TareaFormulario } from './componentes/TareaFormulario';
 import { ListaDeTareas } from './componentes/ListaDeTareas';
@@ -7,12 +6,6 @@ import { ListaDeTareas } from './componentes/ListaDeTareas';
 function App() {
     return (
         <div className="aplicacion-tareas">
-            <div className='freecodecamp-logo-contenedor'>
-                <img
-                    src={logo}
-                    className='freecodecamp-logo'
-                />
-            </div>
             <div className='tareas-lista-principal'>
                 <h1>Mis Tareas</h1>
                 <ListaDeTareas />
