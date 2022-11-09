57fb134a30dda317c92997e582b5824dabbc36d3
Antonio Rufino <devs.toni@gmail.com>
2022-11-09 17:06:12 +0100

    delete search project

--- REACT/react-jonmircha-search/src/components/SongArtist.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const SongArtist = () => {
-  return (
-    <div>
-        <h2>Song Artist</h2>
-    </div>
-  )
-}
--- REACT/react-jonmircha-search/src/components/SongLyrics.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const SongLyrics = () => {
-  return (
-    <div>
-        <h2>Song Lyrics</h2>
-    </div>
-  )
-}
