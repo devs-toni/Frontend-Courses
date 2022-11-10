4c18a3a0bbe8d600bdac33107a2da3c0a92ce190
Antonio Rufino <devs.toni@gmail.com>
2022-11-10 23:33:34 +0100

    react router initialized

---
+++ REACT/react-jonmircha-router-v1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ConceptosBasicos from './components/ConceptosBasicos';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Router</h1>
+      <a href="https://reactrouter.com/web/guides/quick-start">Documentación</a>
+      <hr />
+      <ConceptosBasicos />
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT/react-jonmircha-router-v1/src/components/ConceptosBasicos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConceptosBasicos = () => {
+  return (
+    <div>
+        <h2>Conceptos Básicos</h2>
+    </div>
+  )
+}
+
+export default ConceptosBasicos
