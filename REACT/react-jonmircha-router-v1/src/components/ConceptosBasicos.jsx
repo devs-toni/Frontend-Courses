2137636535efb84aed1d2c5d83d968e7f2c879cc
Antonio Rufino <devs.toni@gmail.com>
2022-11-11 16:48:58 +0100

    hashrouter

--- REACT/react-jonmircha-router-v1/src/components/ConceptosBasicos.jsx
+++ REACT/react-jonmircha-router-v1/src/components/ConceptosBasicos.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Contacto from '../pages/Contacto';
 import Service from '../pages/Service';
 import Home from '../pages/Home';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect, HashRouter, Link } from 'react-router-dom'
 import Error404 from '../pages/Error404';
 import MenuConceptos from './MenuConceptos';
 import Usuario from '../pages/Usuario';
@@ -15,6 +15,21 @@ import PrivateRoute from './PrivateRoute';
 const ConceptosBasicos = () => {
   return (
     <Router>
+      <h2>Hash Router</h2>
+      <HashRouter>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>Acerca</Link>
+          <Link to='/contact'>Contacto</Link>
+        </nav>
+        <Switch>
+          <Route exact={true} path='/' component={Home} />
+          <Route exact={true} path='/about' component={Service} />
+          <Route exact={true} path='/contact' component={Contacto} />
+          <Route exact={true} path='*' component={Error404} />
+        </Switch>
+      </HashRouter>
+      <hr />
       <MenuConceptos />
       <Switch>
         <Route exact={true} path='/' component={Home} />
@@ -30,7 +45,7 @@ const ConceptosBasicos = () => {
         </Route>
         <Route path='/react' component={ReactTopics} />
         <Route exact path='/login' component={Login} />
-        <PrivateRoute exact={true} path='/dashboard' component={Dashboard}/>
+        <PrivateRoute exact={true} path='/dashboard' component={Dashboard} />
         <Route exact={true} path='*' component={Error404} />
       </Switch>
     </Router>
